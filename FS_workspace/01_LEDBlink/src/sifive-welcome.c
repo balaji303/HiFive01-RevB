979383ca338260813511b69e37bce2477b01a61d
Balaji <balajimail@zoho.com>
2023-01-17 16:22:10 +0530

    Lover of the Russian queen

--- FS_workspace/01_LEDBlink/src/sifive-welcome.c
+++ FS_workspace/01_LEDBlink/src/sifive-welcome.c
@@ -1,24 +1,67 @@
-
+/**
+ * @file sifive-welcome.c
+ * @author @balaji303 (https://github.com/balaji303)
+ * @brief Red LED blink code 
+ * @version 1
+ * @date 17-01-2023
+ * 
+ * Copyright (c) 2023 @balaji303
+ * 
+ */
 #include <stdio.h>
 #include <metal/led.h>
 
 void delay(void);
+
+/**
+ * @brief Main Funcion
+ * 
+ * @return int 
+ */
 int main(void)
 {
 	// Declaration
 	struct metal_led *ledRed;
+	/**
+	 * @brief Get the handle for the RedLed
+	 * 
+	 */
 	ledRed = metal_led_get_rgb("LD0", "red");
+	/**
+	 * @brief Construct a new metal led enable object
+	 * 
+	 */
 	metal_led_enable(ledRed);
 	while(1)
 	{
+		/**
+		 * @brief Construct a new metal led off object
+		 * 
+		 */
 		metal_led_off(ledRed);
+		/**
+		 * @brief Construct a new delay object
+		 * 
+		 */
 		delay();
+		/**
+		 * @brief Construct a new metal led on object
+		 * 
+		 */
 		metal_led_on(ledRed);
+		/**
+		 * @brief Construct a new delay object
+		 * 
+		 */
 		delay();
 	}
 	return 0;
 }
 
+/**
+ * @brief A simple delay function
+ * 
+ */
 void delay(void)
 {
 	for(int i=0;i<1000;i++)
